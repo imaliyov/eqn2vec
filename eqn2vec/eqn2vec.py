eb3e7c05f880d8da23d2eb813e9250701e4a5338
imaliyov <ivan.maliyov@gmail.com>
2024-03-15 18:28:27 +0100

    Command line arguments. Output formats: pdf and svg

--- eqn2vec/eqn2vec.py
+++ eqn2vec/eqn2vec.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 import os
 import subprocess
+from latex import build_pdf
+import argparse
 
 
-def check_environment():
+def check_environment(latex_compiler="pdflatex"):
     """
     The following packages are required:
     - pdflatex
@@ -12,11 +14,12 @@ def check_environment():
     """
 
     # Check for pdflatex
-    try:
-        subprocess.run(["pdflatex", "--version"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-    except FileNotFoundError:
-        print("pdflatex not found. Please install TeX Live or MiKTeX.")
-        return False
+    if latex_compiler == 'pdflatex':
+        try:
+            subprocess.run(["pdflatex", "--version"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        except FileNotFoundError:
+            print("pdflatex not found. Please install TeX Live or MiKTeX.")
+            return False
 
     # Check for pdfseparate
     try:
@@ -40,10 +43,6 @@ def create_latex_file(equations, filename="equations.tex"):
     Creates a LaTeX file with each equation on a separate page.
     """
 
-    # equation must be a list
-    if not isinstance(equations, list):
-        equations = list(equations)
-
     with open(filename, "w") as file:
         # Write the LaTeX document header
         file.write("\\documentclass[multi=page, border=1pt]{standalone}\n")
@@ -53,7 +52,7 @@ def create_latex_file(equations, filename="equations.tex"):
         # Add each equation in a separate 'page' environment
         for i, eq in enumerate(equations):
 
-            print(f'Equation {i}: {eq}')
+            print(f'Equation {i+1}: {eq}')
 
             file.write("\\begin{page}\n")
             file.write(f"${eq}$\n")
@@ -65,7 +64,7 @@ def create_latex_file(equations, filename="equations.tex"):
     return filename
 
 
-def compile_latex_to_pdf(latex_file, output_pdf="equations.pdf"):
+def compile_latex_to_pdf_external(latex_file, output_pdf="equations.pdf"):
     """
     Compiles a LaTeX file into a PDF.
     """
@@ -77,35 +76,129 @@ def compile_latex_to_pdf(latex_file, output_pdf="equations.pdf"):
     return output_pdf
 
 
-def split_pdf_to_svgs(pdf_file, equations, output_prefix="eqn"):
+def compile_latex_to_pdf(latex_code, output_pdf="equations.pdf"):
+    """
+    Compiles LaTeX code into a PDF using the `latex` library.
+    """
+    pdf = build_pdf(latex_code)
+    pdf.save_to(output_pdf)
+    return output_pdf
+
+
+def compile_latex_wrapper(latex_file, latex_compiler='latex', output_pdf="equations.pdf"):
+
+    if latex_compiler == 'pdflatex':
+        print('Compiling with pdflatex')
+        compiled_pdf = compile_latex_wrapper(latex_file)
+    elif latex_compiler == 'latex':
+
+        with open(latex_file, "r") as file:
+            latex_code = file.read()
+
+        print('Compiling with Python latex')
+        compiled_pdf = compile_latex_to_pdf(latex_code)
+
+    return compiled_pdf
+
+
+def split_pdf(pdf_file, equations, format='svg', output_prefix="eqn"):
     """
     Splits a PDF into separate PDFs for each page and converts them to SVG files.
     """
+
+    if format not in ['svg', 'pdf']:
+        raise ValueError("Invalid format. Must be 'svg' or 'pdf'.")
+
     # Split the PDF into separate pages
+    print(f'Target format: {format}')
     print(f"Splitting {pdf_file} into separate pages")
     subprocess.run(["pdfseparate", pdf_file, f"{output_prefix}%d.pdf"], check=True)
 
     pdf_file_list = [f"{output_prefix}{i}.pdf" for i in range(1, 1 + len(equations))]
 
-    # Convert each split PDF to SVG
-    for i, pdf in enumerate(pdf_file_list):
-        svg_file = pdf.replace(".pdf", ".svg")
+    if format == 'svg':
+        # Convert each split PDF to SVG
+        for i, pdf in enumerate(pdf_file_list):
+            svg_file = pdf.replace(".pdf", ".svg")
 
-        print(f"Creating {svg_file}")
+            subprocess.run(["pdf2svg", pdf, svg_file], check=True)
 
-        subprocess.run(["pdf2svg", pdf, svg_file], check=True)
+        output_file_list = [pdf.replace(".pdf", ".svg") for pdf in pdf_file_list]
+
+    elif format == 'pdf':
+        output_file_list = pdf_file_list
+
+    for i, file in enumerate(output_file_list):
+        print(f'Output file {i+1}: {file}')
+
+    return output_file_list
+
+
+def cleanup_files(latex_file="equations.tex", pdf_file="equations.pdf", format='svg'):
+
+    print(f'Cleaning up intermediate files.')
+    # Remove the LaTeX file
+    if os.path.exists(latex_file):
+        os.remove(latex_file)
+
+    # Remove the .aux file
+    if os.path.exists(latex_file.replace(".tex", ".aux")):
+        os.remove(latex_file.replace(".tex", ".aux"))
+
+    # Remove the .log file
+    if os.path.exists(latex_file.replace(".tex", ".log")):
+        os.remove(latex_file.replace(".tex", ".log"))
+
+    # Remove the PDF file
+    if os.path.exists(pdf_file):
+        os.remove(pdf_file)
+
+    # Remove the split PDF files
+    if format == 'svg':
+        for i in range(1, 100):
+            pdf_file = f"eqn{i}.pdf"
+            if os.path.exists(pdf_file):
+                os.remove(pdf_file)
+
+
+def eqn2vec(equations, format='svg', keep=False, latex_compiler='latex'):
+
+    # Check equations type
+    if not isinstance(equations, list):
+        equations = [equations]
+
+    check_environment(latex_compiler=latex_compiler)
+
+    latex_file = create_latex_file(equations)
+
+    #latex_compiler = "latex"
+    #latex_compiler = "pdflatex"
+
+    compiled_pdf = compile_latex_wrapper(latex_file, latex_compiler=latex_compiler)
+
+    split_pdf(compiled_pdf, equations, format=format)
+
+    if not keep:
+        cleanup_files(latex_file, compiled_pdf, format=format)
 
 
 def main():
-    eq_list = []
-    eq_list.append("U(\\mathbf{X} ; \\boldsymbol{\\Theta})")
+    #eq_list = []
+    #eq_list.append("U(\\mathbf{X} ; \\boldsymbol{\\Theta})")
+    #eq_list.append("x=y\\tilde{u}")
 
-    # Add more equations as needed
-    # eq_list.append("E = mc^2")
+    # Parse command line arguments
+    parser = argparse.ArgumentParser(
+                description="Convert LaTeX equations to vector image files.")
 
-    latex_file = create_latex_file(eq_list)
-    compiled_pdf = compile_latex_to_pdf(latex_file)
-    split_pdf_to_svgs(compiled_pdf, eq_list)
+    parser.add_argument('-eq', '--equations', required=True, nargs="*", help="The LaTeX equations to convert to vector images.")
+    parser.add_argument('-f', '--format', default="svg", help="The format of the vector image files. Must be 'svg' or 'pdf'.")
+    parser.add_argument('-c', '--compiler', default="latex", help="The LaTeX compiler to use. Must be 'latex' or 'pdflatex'.")
+    parser.add_argument('-k', '--keep', action="store_true", help="Keep the intermediate LaTeX and PDF files.")
+
+    args = parser.parse_args()
+
+    eqn2vec(args.equations, format=args.format.lower(), keep=args.keep, latex_compiler=args.compiler)
 
 
 if __name__ == "__main__":
